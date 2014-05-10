925406451af9df846abbc2f5da27ae6ed874578b
lg188 <lander.ghekiere@gmail.com>
2014-05-10 10:04:05 +0200

    Improved code readabilty and cleaned out useless shit
    Signed-off-by: lg188 <lander.ghekiere@gmail.com>

--- volafile-timestamp.user.js
+++ volafile-timestamp.user.js
@@ -1,21 +1,34 @@
 // ==UserScript==
 // @name        Volafile chat timestamps
-// @namespace   niggerdong.penis
+// @namespace   volafile.improvements
 // @description Adds timestamps to chat messages on Volafile.
-// @match       *://volafile.io/*
-// @include     *://volafile.io/*
-// @version     1
+// @match       http://volafile.io/r/*
+// @include     http://volafile.io/r/*
+// @version     2
 // ==/UserScript==
+
+/*
+ *	@Author: unknown
+ *	@Author: lg188
+ *
+ *	Changelog:
+ *	Version 1:
+ *		Basic script
+ *	Version 2:
+ *		Cleaned out the script
+ **/
+
 var target = document.querySelector('#chat_messages');
 var observer = new MutationObserver(function (mutations) {
-  mutations.forEach(function (mutation) {
-    addTimestamp();
-  });
+	mutations.forEach(function (mutation) {
+		addTimestamp();
+	});
 });
+
 var config = {
-  attributes: true,
-  childList: true,
-  characterData: true
+	attributes: true,
+	childList: true,
+	characterData: true
 };
 var date = new Date();
 var hours = date.getHours();
@@ -23,19 +36,20 @@ var minutes = date.getMinutes();
 var seconds = date.getSeconds();
 var finalTime = hours + ':' + minutes + ':' + seconds;
 function addTimestamp() {
-  date = new Date();
-  hours = date.getHours();
-  minutes = date.getMinutes();
-  seconds = date.getSeconds();
-  if (seconds <= 9) {
-    seconds = '0' + seconds;
-  }
-  if (minutes <= 9) {
-    minutes = '0' + minutes
-  }
-  finalTime = hours + ':' + minutes + ':' + seconds;
-  document.getElementsByClassName('username') [document.getElementsByClassName('username') .length - 1].innerHTML = finalTime + ' | ' + document.getElementsByClassName('username') [document.getElementsByClassName('username') .length - 1].innerHTML;
-  thisfunctiondoesnotexist();
-  //CTRL+F thisfunctiondoesnotexist    1 result (excluding this comment)
+	date = new Date();
+	hours = date.getHours()
+	minutes = date.getMinutes();
+	seconds = date.getSeconds();
+	if (seconds <= 9) {
+		seconds = '0' + seconds;
+	}
+	if (minutes <= 9) {
+		minutes = '0' + minutes
+	}
+
+	finalTime = hours + ':' + minutes + ':' + seconds;
+	document.getElementsByClassName('username') [document.getElementsByClassName('username') .length - 1].innerHTML = finalTime + ' | ' + document.getElementsByClassName('username') [document.getElementsByClassName('username') .length - 1].innerHTML;
+
 }
+
 observer.observe(target, config);
