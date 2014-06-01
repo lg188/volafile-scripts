ba3200903c7d2af67416c78d09ee63d01f72187f
dongmaster <dongmaster@users.noreply.github.com>
2014-06-01 15:25:59 +0200

    Updated volacoin script, added shop and passive gain. Updated chat filter, added a text field for temporary blocking of users

--- volacoin-miner.user.js
+++ volacoin-miner.user.js
@@ -1,30 +1,37 @@
 // ==UserScript==
-// @name        Volacoins 2
-// @namespace   vola.coins
+// @name        Volacoins
+// @namespace   http://volafile.dongmaster.volacoins3
 // @description Click the logo, get money cash volacoins.
 // @match       *://volafile.io/r/*
 // @include     *://volafile.io/r/*
-// @version     1
+// @version     5
 // @grant       none
 // ==/UserScript==
-
 function load(key) { // Easier to type.
 	return JSON.parse(localStorage.getItem(key))
 }
 
-if(load('volacoinKey') === undefined || load('volacoinKey') === null) {
+
+if(load('topcheck') === undefined || load('topcheck') === null) {
     var volacoins = 0;
+    var timerInterval = 5000;
+    localStorage.clear;
     localStorage.setItem('volacoinKey', JSON.stringify(volacoins));
-    console.log('ey');
+    localStorage.setItem('timerIntervalKey', JSON.stringify(timerInterval));
+    localStorage.setItem('topcheck', ('true'));
 } else {
-    var volacoins = localStorage.getItem('volacoinKey');
-    console.log('yo');
+    var volacoins = parseInt(localStorage.getItem('volacoinKey'));
+    var timerInterval = parseInt(localStorage.getItem('timerIntervalKey'));
 }
+
+
+var firstShopItemPrice = 100;
 
 var volacoinHeaderElement = document.createElement('SPAN');
 var volacoinHeaderElementText = document.createTextNode('Volacoin');
 volacoinHeaderElement.appendChild(volacoinHeaderElementText);
 volacoinHeaderElement.setAttribute('class', 'header_row_element');
+volacoinHeaderElement.setAttribute('style', 'user-select: none; -webkit-user-select: none; -moz-user-select: none;')
 //Creating the header element here.
 
 
@@ -60,25 +67,155 @@ function showMenu() {
     }
 }
 
+var showShopMenuCounter = 0;
+
+function showShopMenu() {
+    showShopMenuCounter++;
+    console.log('hey')
+    
+    if(showShopMenuCounter === 1) {
+        shopMenu.style.display = 'block';
+    }
+    
+    if(showShopMenuCounter === 2) {
+        shopMenu.style.display = 'none';
+        showShopMenuCounter = 0;
+    }
+}
+
+
+var clickMultiplier = 1;
+
 function increment() {
-    volacoins++;
+    volacoins += clickMultiplier;
     volacoinAmount.innerHTML='Volacoins: '+volacoins;
     localStorage.setItem('volacoinKey', volacoins);
 }
 
 volacoinHeaderElement.addEventListener('click', showMenu, false);
 
+
+var multiplier = 1;
+
+function updateVolacoinText() {
+    volacoinAmount.innerHTML='Volacoins: '+volacoins;
+    volacoinAmount2.innerHTML='Volacoins: '+volacoins;
+    localStorage.setItem('volacoinKey', volacoins);
+}
+
+function increment_passive() {
+    setTimeout(function() {
+        volacoins += multiplier;
+        increment_passive();
+        updateVolacoinText();
+    }, timerInterval);
+}
+
+increment_passive();
+
+function buyFirst() {
+    if(volacoins >= 100 && timerInterval > 1) {
+        volacoins -= 100;
+        timerInterval--;
+        localStorage.setItem('timerIntervalKey', timerInterval);
+        updateVolacoinText();
+    }
+}
+
+var clickableFrame = document.createElement('DIV');
+clickableFrame.setAttribute('style', 'margin-left: auto; margin-right: auto; display: block; margin-top: 40px;');
+
+volacoinMenu.appendChild(clickableFrame);
+
 var clickable = document.createElement('IMG');
 clickable.setAttribute('src', 'http://volafile.io/static/logo_mid.png');
-clickable.setAttribute('style', 'margin-left: auto; margin-right: auto; display: block; margin-top: 60px;');
-volacoinMenu.appendChild(clickable);
+clickable.setAttribute('style', 'margin-left: auto; margin-right: auto; display: block; margin-top: 40px;');
+clickableFrame.appendChild(clickable);
 
 clickable.addEventListener('click', increment, false);
 
 
 var volacoinAmount = document.createElement('P');
 var volacoinAmountText = document.createTextNode('Volacoins: '+volacoins);
-volacoinAmount.setAttribute('style', 'position: absolute; bottom: 0;')
+//volacoinAmount.setAttribute('style', 'position: absolute; bottom: 0;')
 volacoinAmount.appendChild(volacoinAmountText);
 
 volacoinMenu.appendChild(volacoinAmount);
+
+
+var showShopButton = document.createElement('BUTTON');
+var showShopButtonText = document.createTextNode('Shop');
+showShopButton.appendChild(showShopButtonText);
+showShopButton.setAttribute('class', 'button');
+showShopButton.setAttribute('style', 'margin-bottom: 20px;')
+
+volacoinMenu.appendChild(showShopButton);
+showShopButton.addEventListener('click', showShopMenu, false);
+
+var closeVolacoinMenuButton = document.createElement('BUTTON');
+var closeVolacoinMenuButtonText = document.createTextNode('Close');
+closeVolacoinMenuButton.appendChild(closeVolacoinMenuButtonText);
+closeVolacoinMenuButton.setAttribute('class', 'button');
+closeVolacoinMenuButton.setAttribute('style', 'position: absolute; bottom: 13px; right: 10px;');
+volacoinMenu.appendChild(closeVolacoinMenuButton);
+
+closeVolacoinMenuButton.addEventListener('click', showMenu, false);
+
+var shopMenu = document.createElement('DIV');
+shopMenu.setAttribute('class', 'ui_frame');
+shopMenu.setAttribute('style', 'width: 700px; height: 55%; display: none; margin-left: auto; margin-right: auto; position: relative; top: -144px;')
+document.body.appendChild(shopMenu);
+
+
+var shopBackButton = document.createElement('BUTTON');
+var shopBackButtonText = document.createTextNode('Close');
+shopBackButton.appendChild(shopBackButtonText);
+shopBackButton.setAttribute('class', 'button');
+shopBackButton.setAttribute('style', 'position: absolute; bottom: 10px; right: 10px;')
+shopMenu.appendChild(shopBackButton);
+
+shopBackButton.addEventListener('click', showShopMenu, false);
+
+var shopMenuContentContainer = document.createElement('DIV');
+shopMenuContentContainer.setAttribute('class', 'ui_frame_content');
+shopMenu.appendChild(shopMenuContentContainer);
+
+var volacoinAmount2 = document.createElement('P');
+var volacoinAmount2Text = document.createTextNode('Volacoins: '+volacoins);
+volacoinAmount2.setAttribute('style', 'position: absolute; top: 0;')
+volacoinAmount2.appendChild(volacoinAmount2Text);
+
+shopMenuContentContainer.appendChild(volacoinAmount2);
+
+var shopMenuTable = document.createElement('TABLE');
+var shopMenuTableTR = document.createElement('TR');
+var shopMenuTableTD_1 = document.createElement('TD');
+var shopMenuTableTD_2 = document.createElement('TD');
+shopMenuTable.setAttribute('class', 'pretty_table');
+shopMenuTable.style.marginTop = '50px';
+shopMenuTable.appendChild(shopMenuTableTR);
+shopMenuTable.appendChild(shopMenuTableTD_1);
+shopMenuTable.appendChild(shopMenuTableTD_2);
+shopMenuContentContainer.appendChild(shopMenuTable);
+
+var shopItem_1 = document.createElement('P');
+var shopItemText_1 = document.createTextNode('1. Faster passive gain. Price: '+firstShopItemPrice);
+shopItem_1.appendChild(shopItemText_1);
+shopMenuTableTD_1.appendChild(shopItem_1);
+
+var shopItemButton_1 = document.createElement('BUTTON');
+var shopItemButtonText_1 = document.createTextNode('Buy');
+shopItemButton_1.setAttribute('class', 'button');
+shopItemButton_1.appendChild(shopItemButtonText_1);
+
+shopMenuTableTD_2.appendChild(shopItemButton_1)
+
+shopItemButton_1.addEventListener('click', buyFirst, false);
+
+
+
+
+
+
+
+
--- volafile-chat-filter.user.js
+++ volafile-chat-filter.user.js
@@ -1,21 +1,55 @@
 // ==UserScript==
 // @name        Volafile chat filter
 // @namespace   keystrokes.se
-// @description Is someone shitposting in chat? Block them with this script!
+// @description Is someone shitposting in chat? Block them with this script
 // @match       *://volafile.io/*
 // @include     *://volafile.io/*
-// @version     5
+// @version     6
 // @author      Dongmaster
 // @author      cyberia
 // ==/UserScript==
-
-
 
 //Edit the variables below to your liking!
 var filtered = []; //This is the blacklist for registered users AND unregistered users. Use this if you don't want to block all unregistered users! The format for filtering users: /username/i    if you want to filter several users: /user1/i, /user2/i
 var unregisteredWhitelist = []; //(Variable below must be enabled for this) This is the whitelist for unregistered users. The format for whitelisting users is; /username/i     If you want several users: /user1/i, /user2/i
 var filterUnregistered = false; //Change to true if you want to filter ALL unregistered users (users in the whitelist are not filtered when this is enabled).
 //Edit the variables above to your liking!
+
+function id(selector) {
+    return document.getElementById(selector);
+}
+
+var textfieldSpan = document.createElement('SPAN');
+textfieldSpan.setAttribute('id', 'chat_filter_span');
+textfieldSpan.setAttribute('class', 'header_row_element');
+
+id('header_row2').appendChild(textfieldSpan);
+
+var textfield = document.createElement('INPUT');
+textfield.setAttribute('id', 'filter_input');
+textfield.setAttribute('type', 'text');
+textfield.style.backgroundColor = '#264559';
+textfield.style.height = '1.2em';
+textfield.style.verticalAlign = 'bottom';
+textfield.setAttribute('placeholder', 'Add user to filter');
+textfield.style.width = '145px';
+textfield.setAttribute('autocomplete', 'off');
+
+textfieldSpan.appendChild(textfield);
+
+var tempSpecUser;
+var user;
+
+textfield.addEventListener('keyup', function(e) {
+    if(e.keyCode === 13) {
+       tempUser = id('filter_input').value;
+       user = new RegExp(tempUser, 'i');
+        
+       filtered.push(user);
+       
+       id('filter_input').value = '';
+    }
+}, false);
 
 var observer = new MutationObserver(function(mutations){
 	mutations.forEach(function(mutation){
@@ -29,7 +63,7 @@ var observer = new MutationObserver(function(mutations){
 				}
 				//console.log(mutation.addedNodes[j].getElementsByClassName('username')[0].innerHTML);
 			}
-			console.log(unregisteredWhitelist.length);
+            
             if(unregisteredWhitelist.length === 0) {
                 for(k = 0; k < unregisteredWhitelist.length+1; k++) {
             		if(filterUnregistered === true && nameNode.hasAttribute('href') === false && !/messagelog/i.test(nameNode.innerHTML) && !/room/i.test(nameNode.innerHTML) && !/system/i.test(nameNode.innerHTML) && !/network/i.test(nameNode.innerHTML) && !/motd/i.test(nameNode.innerHTML)) {
@@ -46,5 +80,7 @@ var observer = new MutationObserver(function(mutations){
 		}
 	});
 });
+
+
 
 observer.observe(document.querySelector('#chat_messages'), {childList: true});
